a2225e3a3351db9304609d39ef75102476447170
Simon Minton <simon@minton.co>
2022-10-19 16:19:16 +0100

    auto refresh

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ error_reporting(0);
         <title>Trains To Richmond</title>
         <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="refresh" content="30">
   <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body class="bg-black">
         <div class="text-white py-2 font-bold text-center">SOUTH KENSINGTON</div>
-<ul class="bg-gray-900 px-8 py-6 max-w-md w-full mx-auto font-mono">
+<ul class="bg-gray-900 px-4 md:px-8 py-6 max-w-md w-full mx-auto font-mono">
     <?php
 $file = file_get_contents('https://api.tfl.gov.uk/Line/district/Arrivals/940GZZLUSKS?direction=inbound&destinationStationId=940GZZLURMD');
 // Decode the JSON into an associative array
@@ -33,6 +34,6 @@ if($seconds < 10) {
     echo '<div class="w-full text-amber-400 text-sm">'.$train['currentLocation']."</div></li>";
 }
 ?>
-</ul>
+
     </body>
 </html>
