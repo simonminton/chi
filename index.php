8786b9c185662e37e4a027192be42fd65331eb18
Simon Minton <simon@minton.co>
2022-10-19 16:21:57 +0100

    reduce auto refresh time

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ error_reporting(0);
         <title>Trains To Richmond</title>
         <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="refresh" content="30">
+  <meta http-equiv="refresh" content="15">
   <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body class="bg-black">
